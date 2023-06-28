83d55206030bd514f4b976c4801f0b375c28807c
ge <gechandev@gmail.com>
2023-06-28 16:04:56 +0300

    add k8s commands

--- twc/commands/kubernetes.py
+++ twc/commands/kubernetes.py
@@ -10,6 +10,7 @@ from requests import Response
 
 from twc import fmt
 from twc.typerx import TyperAlias
+from twc.api import TimewebCloud
 from twc.apiwrap import create_client
 from .common import (
     verbose_option,
@@ -22,8 +23,8 @@ from .common import (
 
 
 cluster = TyperAlias(help=__doc__)
-cluster_group = TyperAlias(help="Manage cluster node groups.")
-cluster_node = TyperAlias(help="Manage cluster nodes.")
+cluster_group = TyperAlias(help="Manage worker node groups.")
+cluster_node = TyperAlias(help="Manage worker nodes.")
 cluster.add_typer(cluster_group, name="group", aliases=["groups"])
 cluster.add_typer(cluster_node, name="node", aliases=["nodes"])
 
@@ -224,7 +225,7 @@ def cluster_kubeconfig(
         help="Path to file. NOTE: Existing file will be overwitten.",
     ),
 ):
-    """Download kubeconfig.yaml."""
+    """Download KubeConfig."""
     client = create_client(config, profile)
     file_content = client.get_k8s_cluster_kubeconfig(cluster_id).text
     if save:
@@ -432,3 +433,207 @@ def cluster_group_list(
         output_format=output_format,
         func=print_node_groups,
     )
+
+
+# ------------------------------------------------------------- #
+# $ twc cluster group add                                       #
+# ------------------------------------------------------------- #
+
+
+@cluster_group.command("add")
+def cluster_group_add(
+    cluster_id: int,
+    verbose: Optional[bool] = verbose_option,
+    config: Optional[Path] = config_option,
+    profile: Optional[str] = profile_option,
+    output_format: Optional[str] = output_format_option,
+    name: str = typer.Option(..., help="Node group display name."),
+    preset_id: int = typer.Option(..., help="Nodes configuration preset."),
+    nodes: int = typer.Option(1, help="Number of nodes in group."),
+):
+    """Add new node group."""
+    client = create_client(config, profile)
+    response = client.create_k8s_node_group(
+        cluster_id,
+        name=name,
+        preset_id=preset_id,
+        node_count=nodes,
+    )
+    fmt.printer(
+        response,
+        output_format=output_format,
+        func=lambda response: print(response.json()["node_group"]["id"]),
+    )
+
+
+# ------------------------------------------------------------- #
+# $ twc cluster group remove                                    #
+# ------------------------------------------------------------- #
+
+
+def get_cluster_id_by_group(client: TimewebCloud, group_id: int) -> int:
+    clusters = client.get_k8s_clusters().json()["clusters"]
+    for _cluster in clusters:
+        groups = client.get_k8s_node_groups(_cluster["id"]).json()[
+            "node_groups"
+        ]
+        for group in groups:
+            if group_id == group["id"]:
+                return _cluster["id"]
+    sys.exit(f"Error: Node group '{group_id}' not found.")
+
+
+@cluster_group.command("remove", "rm")
+def cluster_group_remove(
+    group_ids: List[int] = typer.Argument(..., metavar="GROUP_ID..."),
+    verbose: Optional[bool] = verbose_option,
+    config: Optional[Path] = config_option,
+    profile: Optional[str] = profile_option,
+    yes: Optional[bool] = yes_option,
+):
+    """Remove node group."""
+    if not yes:
+        typer.confirm("This action cannot be undone. Continue?", abort=True)
+    client = create_client(config, profile)
+    for group_id in group_ids:
+        cluster_id = get_cluster_id_by_group(client, group_id)
+        response = client.delete_k8s_node_group(cluster_id, group_id)
+        if response.status_code == 204:
+            print(group_id)
+        else:
+            sys.exit(fmt.printer(response))
+
+
+# ------------------------------------------------------------- #
+# $ twc cluster group scale-up                                  #
+# ------------------------------------------------------------- #
+
+
+@cluster_group.command("scale-up")
+def cluster_group_scaleup(
+    group_id: int,
+    count: Optional[int] = typer.Argument(1, help="Nodes count."),
+    verbose: Optional[bool] = verbose_option,
+    config: Optional[Path] = config_option,
+    profile: Optional[str] = profile_option,
+    output_format: Optional[str] = output_format_option,
+):
+    """Add worker nodes to group."""
+    client = create_client(config, profile)
+    cluster_id = get_cluster_id_by_group(client, group_id)
+    response = client.add_k8s_nodes_to_group(cluster_id, group_id, count)
+    fmt.printer(
+        response,
+        output_format=output_format,
+        func=lambda response: print(
+            f"Nodes in group {group_id}:", response.json()["meta"]["total"]
+        ),
+    )
+
+
+# ------------------------------------------------------------- #
+# $ twc cluster group scale-down                                #
+# ------------------------------------------------------------- #
+
+
+@cluster_group.command("scale-down")
+def cluster_group_scaledown(
+    group_id: int,
+    count: Optional[int] = typer.Argument(1, help="Nodes count."),
+    verbose: Optional[bool] = verbose_option,
+    config: Optional[Path] = config_option,
+    profile: Optional[str] = profile_option,
+    output_format: Optional[str] = output_format_option,
+):
+    """Remove worker nodes from group."""
+    client = create_client(config, profile)
+    cluster_id = get_cluster_id_by_group(client, group_id)
+    response = client.delete_k8s_nodes_from_group(cluster_id, group_id, count)
+    fmt.printer(
+        response,
+        output_format=output_format,
+        func=lambda response: print(
+            f"Nodes in group {group_id}:", response.json()["meta"]["total"]
+        ),
+    )
+
+
+# ------------------------------------------------------------- #
+# $ twc cluster node list                                       #
+# ------------------------------------------------------------- #
+
+
+def print_nodes(response: Response):
+    nodes = response.json()["nodes"]
+    table = fmt.Table()
+    table.header(
+        [
+            "ID",
+            "GROUP",
+            "TYPE",
+            "STATUS",
+            "CREATED",
+            "CPU/RAM/DISK",
+        ]
+    )
+    for node in nodes:
+        table.row(
+            [
+                node["id"],
+                node["group_id"],
+                node["type"],
+                node["status"],
+                node["created_at"],
+                str(node["cpu"])
+                + "/"
+                + str(round(node["ram"] / 1024))
+                + "G/"
+                + str(node["disk"])
+                + "G",
+            ]
+        )
+    table.print()
+
+
+@cluster_node.command("list", "ls")
+def cluster_node_list(
+    cluster_id: int,
+    verbose: Optional[bool] = verbose_option,
+    config: Optional[Path] = config_option,
+    profile: Optional[str] = profile_option,
+    output_format: Optional[str] = output_format_option,
+):
+    """List cluster node groups."""
+    client = create_client(config, profile)
+    response = client.get_k8s_nodes(cluster_id)
+    fmt.printer(
+        response,
+        output_format=output_format,
+        func=print_nodes,
+    )
+
+
+# ------------------------------------------------------------- #
+# $ twc cluster node remove                                     #
+# ------------------------------------------------------------- #
+
+
+@cluster_node.command("remove", "rm")
+def cluster_node_remove(
+    cluster_id: int,
+    node_ids: List[int] = typer.Argument(..., metavar="NODE_ID..."),
+    verbose: Optional[bool] = verbose_option,
+    config: Optional[Path] = config_option,
+    profile: Optional[str] = profile_option,
+    yes: Optional[bool] = yes_option,
+):
+    """Remove nodes from cluster."""
+    if not yes:
+        typer.confirm("This action cannot be undone. Continue?", abort=True)
+    client = create_client(config, profile)
+    for node_id in node_ids:
+        response = client.delete_k8s_node(cluster_id, node_id)
+        if response.status_code == 204:
+            print(node_id)
+        else:
+            sys.exit(fmt.printer(response))
